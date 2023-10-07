1f5d54e5b692dc5eb07c25db74556ea5da38327a
gleyson003 <lop23031@byui.edu>
2023-10-07 08:48:13 -0300

    week 4

--- sentences.py
+++ sentences.py
@@ -130,10 +130,60 @@ def make_sentence(quantity, tense):
     determiner = get_determiner(quantity)
     noun = get_noun(quantity)
     verb = get_verb(quantity, tense)
+    adjective = get_adjective()
 
-    show_sentence = f"{determiner.capitalize()} {noun} {verb}."
+    preposition_phrase1 = get_prepositional_phrase(quantity)
+    preposition_phrase2 = get_prepositional_phrase(quantity)
+    show_sentence = f"{determiner.capitalize()} {adjective} {noun} {preposition_phrase2} {verb} {preposition_phrase1}."
 
     return show_sentence
+
+def get_preposition():
+    """Return a randomly chosen preposition
+    from this list of prepositions:
+        "about", "above", "across", "after", "along",
+        "around", "at", "before", "behind", "below",
+        "beyond", "by", "despite", "except", "for",
+        "from", "in", "into", "near", "of",
+        "off", "on", "onto", "out", "over",
+        "past", "to", "under", "with", "without"
+
+    Return: a randomly chosen preposition.
+    """
+
+    words = ["about", "above", "across", "after", "along", "around", "at", "before", "behind", "below",
+        "beyond", "by", "despite", "except", "for", "from", "in", "into", "near", "of",
+        "off", "on", "onto", "out", "over", "past", "to", "under", "with", "without"]
+    
+    word = random.choice(words)
+    return word
+
+def get_prepositional_phrase(quantity):
+    """Build and return a prepositional phrase composed
+    of three words: a preposition, a determiner, and a
+    noun by calling the get_preposition, get_determiner,
+    and get_noun functions.
+
+    Parameter
+        quantity: an integer that determines if the
+            determiner and noun in the prepositional
+            phrase returned from this function should
+            be single or pluaral.
+    Return: a prepositional phrase.
+    """
+
+    preposition = get_preposition()
+    determiner = get_determiner(quantity)
+    noun = get_noun(quantity)
+    preposition_phrase = f"{preposition} {determiner} {noun}"
+
+    return preposition_phrase
+
+def get_adjective():
+    words = ['small', 'big', 'crazy', 'beautiful', 'strong', 'running', 'sad', 'happy']
+    word = random.choice(words)
+
+    return word
 
 main()
 
