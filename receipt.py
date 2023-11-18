fc6e9a7f67911b78373cce6f2fcaefe15d6bef0d
gleyson003 <lop23031@byui.edu>
2023-11-18 10:30:03 -0300

    week 9

--- receipt.py
+++ receipt.py
@@ -1,38 +1,69 @@
 import csv
+from datetime import datetime
 
 def main():
 
+    current_date_and_time = datetime.now()
     product_dict = read_dictionary("products.csv", 0)
-    print('All Products')
-    print(product_dict)
 
-    print('\nRequested Items')
-    with open("request.csv", "rt") as file:
-        reader = csv.reader(file)
-        next(reader)
+    print('\nInkom Emporium\n')
 
-        for row in reader:
-            PRODUCT_CODE = row[0]
-            PRODUCT_DATA = product_dict[PRODUCT_CODE]
+    try:
+        with open("request.csv", "rt") as file:
+            reader = csv.reader(file)
+            next(reader)
 
-            PRODUCT_NAME = PRODUCT_DATA[1]
-            PRODUCT_QUANTITY = 1
-            PRODUCT_PRICE = PRODUCT_DATA[2]
+            number_of_items = 0
+            subtotal = 0
+            tax = 0.06
 
-            print(f'{PRODUCT_NAME}: {row[PRODUCT_QUANTITY]} @ {PRODUCT_PRICE}')
+            for row in reader:
+                PRODUCT_CODE = row[0]
+
+                try:
+                    PRODUCT_DATA = product_dict[PRODUCT_CODE]
+                    PRODUCT_NAME = PRODUCT_DATA[1]
+                    PRODUCT_QUANTITY = 1
+                    PRODUCT_PRICE = PRODUCT_DATA[2]
+
+                    number_of_items += int(row[PRODUCT_QUANTITY])
+                    subtotal += float(PRODUCT_PRICE) * int(row[PRODUCT_QUANTITY])
+
+                    print(f'{PRODUCT_NAME}: {row[PRODUCT_QUANTITY]} @ {PRODUCT_PRICE}')
+
+                except KeyError as err_key:
+                    error_message = f"Error: unknown product ID in the request.csv file\n{err_key}"
+                    print(error_message)
+
+            sales_tax = subtotal * tax
+            discount_amount = 0
+
+            if current_date_and_time.weekday() in [1, 2]:
+                discount = 0.10
+                discount_amount = subtotal * discount
+                total = subtotal + sales_tax - discount_amount
+                print(f'\nDiscount applied (10%) for Tuesday or Wednesday')
+            else:
+                total = subtotal + sales_tax
+
+            print(
+                f'\nNumber of items: {number_of_items}\n'
+                f'Subtotal: {subtotal:.2f}\n'
+                f'Sales Tax: {sales_tax:.2f}\n'
+                f'Discount: {discount_amount:.2f}\n'
+                f'Total: {total:.2f}\n'
+            )
+
+            print(
+                f"Thank you for shopping at the Inkom Emporium.\n"
+                f"{current_date_and_time:%a %b %d %H:%M:%S %Y}"
+            )
+
+    except FileNotFoundError as not_found_err:
+        print('Error: missing file')
+        print(not_found_err)
 
 def read_dictionary(filename, key_column_index):
-    """Read the contents of a CSV file into a compound
-    dictionary and return the dictionary.
-
-    Parameters
-        filename: the name of the CSV file to read.
-        key_column_index: the index of the column
-            to use as the keys in the dictionary.
-    Return: a compound dictionary that contains
-        the contents of the CSV file.
-    """
-
     dictionary = {}
 
     with open(filename, "rt") as csv_file:
